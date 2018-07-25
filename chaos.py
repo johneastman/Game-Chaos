7e085602f8d9005b4f09d6e24977d1828920cfc3
johneastman <Contr@p05itive>
2018-07-25 12:26:12 -0400

    Added main

--- chaos.py
+++ chaos.py
@@ -10,7 +10,7 @@ class Point:
     def __init__(self):
         self.radius = 10
         self.divisor = 9
-        
+
         self.lower_bound = 1 / self.divisor
         self.upper_bound = (self.divisor - 1) / self.divisor
 
@@ -40,13 +40,13 @@ def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
-    
+
 def message_display(text, pos):
     largeText = pygame.font.Font('freesansbold.ttf', 40)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = (pos)
     display.blit(TextSurf, TextRect)
-    
+
 start = (400, 300)  # Change starting point here
 current_position = start
 
@@ -65,12 +65,12 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-            
+
     random_number = random.randint(1, 6)
-    
+
     # The formula for the middle point of a line drawn between two points
     # is M = ((x1 + x2) / 2, (y1 + y2) / 2)
-    
+
     if random_number in [1, 2]:
         # A[0] == x; A[1] == y
         x = round((A[0] + current_position[0]) / 2)
@@ -78,12 +78,12 @@ while True:
 
         current_position = (x, y)
         dots.append(current_position)
-        
+
     elif random_number in [3, 4]:
         # B[0] == x; B[1] == y
         x = round((B[0] + current_position[0]) / 2)
         y = round((B[1] + current_position[1]) / 2)
-        
+
         current_position = (x, y)
         dots.append(current_position)
 
@@ -94,27 +94,28 @@ while True:
 
         current_position = (x, y)
         dots.append(current_position)
-    
+
     display.fill(white)
-    
+
     for dot in dots:
         pygame.draw.circle(display, black, dot, dot_size)
-        
+
     message_display("A", display_a)
     pygame.draw.circle(display, red, A, point.radius)
-    
+
     message_display("B", display_b)
     pygame.draw.circle(display, red, B, point.radius)
-    
+
     message_display("C", display_c)
     pygame.draw.circle(display, red, C, point.radius)
-    
+
     # Starting Position
     pygame.draw.circle(display, green, start, point.radius)
-    
-    
+
+
     pygame.display.update()
     clock.tick(FRAMES)
 
-game_loop()
-pygame.quit()
+if __name__ == "__main__":
+    game_loop()
+    pygame.quit()
